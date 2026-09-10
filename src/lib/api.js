60784fb544040b30a7f63d273215c1893db1aa65
Abeysiri A.D.R.V. <vishmika2adr@gmail.com>
2026-09-10 07:05:00 +0530

    changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:8080/api";
-
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 // ========== Helper ==========
 function getAuthHeaders() {
   const token = localStorage.getItem("token");
